2a5678ec7dee598eea22c57030c7754a42b78a4c
hextra <nicolas.rapuzzi-pisarczyk@etu.umontpellier.fr>
2025-06-18 10:10:37 +0200

    aled

--- includes/config.php
+++ includes/config.php
@@ -1,9 +1,21 @@
 <?php
+$host = 'localhost';
+$dbname = 'nicolas.rapuzzi';
+$charset = 'utf8mb4';
+$user = 'nicolas.rapuzzi';
+$password = 'D]6LYvSr7hT40fF[';
+
+$dsn = "mysql:host=$host;dbname=$dbname;charset=$charset";
+
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // Active les exceptions
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, // Mode de récupération par défaut
+    PDO::ATTR_EMULATE_PREPARES => false, // Prépare les requêtes côté serveur
+];
+
 try {
-    $db = new PDO('mysql:host=localhost;dbname=nicolas.rapuzzi;charset=utf8', 'nicolas.rapuzzi', 'D]6LYvSr7hT40fF[
-');
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (Exception $e) {
-    die('Erreur de connexion : ' . $e->getMessage());
+    $pdo = new PDO($dsn, $user, $password, $options);
+} catch (PDOException $e) {
+    die("Erreur de connexion à la base de données : " . $e->getMessage());
 }
 ?>
